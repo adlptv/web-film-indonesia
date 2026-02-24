07e010b9c91eea8517d5a78f96a31b71ec7233f4
Your Name <you@example.com>
2026-02-24 19:09:57 +0700

    Monolithic Backend Fix: Consistently serve all endpoints from a single api/index.ts file to ensure Vercel compatibility

--- api/_lib/util/clean-text.ts
+++
@@ -1,6 +0,0 @@
-import * as cheerio from "cheerio";
-
-export const cleanText = (text: string | undefined): string => {
-  const $ = cheerio.load(`<div>${text}</div>`);
-  return $("div").text().replace(/\s+/g, " ").trim();
-};
